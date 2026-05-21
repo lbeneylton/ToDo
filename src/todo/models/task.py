4ee0d37d3cd9aeb12f145edc277a359c5782005f
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-05-21 00:07:57 -0300

    dev: modelo das tasks

---
+++ src/todo/models/task.py
@@ -0,0 +1,37 @@
+# Importação da Base
+from core.base import Base
+
+# Classes e metodos do ORM para modelagem
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, func  # Tipos do SqlAlchemy
+
+
+# Classe para modelo de task no banco de dados
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # Dados da task criados pelo usuario
+    titulo: Mapped[str] = mapped_column(String(30), nullable=False)
+    descricao: Mapped[str] = mapped_column(String(300), default="")
+
+    # Dados da task criados pelo sistema
+    data_criacao: Mapped[str] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
+    data_atualizacao: Mapped[str] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False
+    )
+
+    # Estado da task (Pendente, Concluida)
+    status: Mapped[str] = mapped_column(
+        String(20),
+        default="pendente",
+        nullable=False
+    )
