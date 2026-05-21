24c2e867efec9825711b46d52fd369abbd7b3dd6
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-05-21 01:17:13 -0300

    dev: classe de logger criada

---
+++ src/todo/core/logger.py
@@ -0,0 +1,60 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+
+class Logger:
+    """Wrapper padrão para logs do sistema"""
+
+    def __init__(
+        self,
+        name: str = "todo_log",
+        log_dir: str = "/src/logs",
+        log_file: str = "log"
+    ):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+
+        # evita duplicar handlers
+        if self.logger.handlers:
+            return
+
+        # cria pasta de logs se não existir
+        os.makedirs(log_dir, exist_ok=True)
+
+        log_path = os.path.join(log_dir, log_file)
+
+        formatter = logging.Formatter(
+            "[%(asctime)s] %(levelname)s - %(name)s - %(message)s"
+        )
+
+        # console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+
+        # file handler com rotação
+        file_handler = RotatingFileHandler(
+            log_path,
+            maxBytes=5 * 1024 * 1024,  # 5MB
+            backupCount=3
+        )
+        file_handler.setFormatter(formatter)
+
+        # adiciona handlers
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
+
+    def info(self, message: str, **kwargs):
+        self.logger.info(message, extra=kwargs)
+
+    def error(self, message: str, **kwargs):
+        self.logger.error(message, extra=kwargs)
+
+    def warning(self, message: str, **kwargs):
+        self.logger.warning(message, extra=kwargs)
+
+    def debug(self, message: str, **kwargs):
+        self.logger.debug(message, extra=kwargs)
+
+
+logger = Logger()
